a26d6be9011ca4c381998ad538553f43034984a0
dhaveed <davidadegoke31@gmail.com>
2018-08-20 18:34:17 +0100

    refactoring, style updates, structure modif

--- register.php
+++ register.php
@@ -7,18 +7,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Sunxcoin Affiliate Registration</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="../../vendors/iconfonts/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="../../vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">
-  <link rel="stylesheet" href="../../vendors/iconfonts/simple-line-icon/css/simple-line-icons.css">
-  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
-  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.addons.css">
+  <link rel="stylesheet" href="./main/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="./main/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">
+  <link rel="stylesheet" href="./main/vendors/iconfonts/simple-line-icon/css/simple-line-icons.css">
+  <link rel="stylesheet" href="./main/vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="./main/vendors/css/vendor.bundle.addons.css">
+  <link rel="stylesheet" href="./main/css/custom.css">
+  <link rel="stylesheet" href="./main/css/loading.css">
+  <link rel="stylesheet" href="./main/css/loading-btn.css">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./main/css/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="./images/favicon.png" />
+  <link rel="shortcut icon" href="./main/images/favicon.png" />
 </head>
 
 <body>
@@ -93,15 +96,15 @@
   </div>
   <!-- container-scroller -->
   <!-- plugins:js -->
-  <script src="../../vendors/js/vendor.bundle.base.js"></script>
-  <script src="../../vendors/js/vendor.bundle.addons.js"></script>
+  <script src="./main/vendors/js/vendor.bundle.base.js"></script>
+  <script src="./main/vendors/js/vendor.bundle.addons.js"></script>
   <!-- endinject -->
   <!-- inject:js -->
-  <script src="../../js/off-canvas.js"></script>
-  <script src="../../js/hoverable-collapse.js"></script>
-  <script src="../../js/misc.js"></script>
-  <script src="../../js/settings.js"></script>
-  <script src="../../js/todolist.js"></script>
+  <script src="./main/js/off-canvas.js"></script>
+  <script src="./main/js/hoverable-collapse.js"></script>
+  <script src="./main/js/misc.js"></script>
+  <script src="./main/js/settings.js"></script>
+  <script src="./main/js/todolist.js"></script>
   <!-- endinject -->
 </body>
 
